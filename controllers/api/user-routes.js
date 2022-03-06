500fc692f158ef1ecc50c6ab23bec2951c0461f5
Lewis <dsapione@gmail.com>
2022-03-06 12:34:59 -0500

    fixed all bugs for comments

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote, Comment } = require("../../models");
+const { User, Post, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -14,33 +14,32 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-	User.findOne({
-		attributes: { exclude: ['password'] },
-		where: {
-			id: req.params.id
-		},
-		include: [
-			{
-				model: Post,
-				attributes: ['id', 'title', 'post_url', 'created_at']
-			},
-			// include the Comment model here:
-			{
-				model: Comment,
-				attributes: ['id', 'comment_text', 'created_at'],
-				include: {
-					model: Post,
-					attributes: ['title']
-				}
-			},
-			{
-				model: Post,
-				attributes: ['title'],
-				through: Vote,
-				as: 'voted_posts'
-			}
-		]
-	})
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
+      }
+    ]
+  })
     .then(dbUserData => {
       if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
@@ -61,22 +60,23 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-	.then(dbUserData => {
-		req.session.save(() => {
-			req.session.user_id = dbUserData.id;
-			req.session.username = dbUserData.username;
-			req.session.loggedIn = true;
-	
-			res.json(dbUserData);
-		});
-	})
-	.catch(err => {
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
 router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email
@@ -95,18 +95,17 @@ router.post('/login', (req, res) => {
     }
 
     req.session.save(() => {
-      // declare session variables
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
-
+  
       res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
   });
 });
 
 router.post('/logout', (req, res) => {
-	if (req.session.loggedIn) {
+  if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
@@ -127,7 +126,7 @@ router.put('/:id', (req, res) => {
     }
   })
     .then(dbUserData => {
-      if (!dbUserData[0]) {
+      if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Vote } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
-	console.log(req.session);
+  console.log('======================');
   Post.findAll({
     attributes: [
       'id',
@@ -28,12 +29,12 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
-      // pass a single post object into the homepage template
-			const posts = dbPostData.map(post => post.get({ plain: true }));
-			res.render('homepage', {
-				posts,
-				loggedIn: req.session.loggedIn
-			});
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
@@ -41,15 +42,7 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-
-  res.render('login');
-});
-
+// get single post
 router.get('/post/:id', (req, res) => {
   Post.findOne({
     where: {
@@ -83,19 +76,26 @@ router.get('/post/:id', (req, res) => {
         return;
       }
 
-      // serialize the data
       const post = dbPostData.get({ plain: true });
 
-      // pass data to template
       res.render('single-post', {
-				post,
-				loggedIn: req.session.loggedIn
-			});
+        post,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
