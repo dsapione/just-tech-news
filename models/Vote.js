cb88104f3653cfb4689ea3bda64865d733bb932c
Lewis <dsapione@gmail.com>
2022-03-06 10:09:50 -0500

    fixed bug duplicating email

--- models/Vote.js
+++ models/Vote.js
@@ -10,24 +10,21 @@ Vote.init(
       primaryKey: true,
       autoIncrement: true
     },
-    // What needs to go here?
-		user_id: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			references: {
-				model: 'user',
-				key: 'id'
-			}
-		},
-		post_id: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			references: {
-				model: 'post',
-				key: 'id'
-			}
-		}
-	},
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
+  },
   {
     sequelize,
     timestamps: false,
